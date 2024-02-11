2f342f724ff6fc08c3fca5be9299de21908eb496
wael <wael.abdallah@etudiant-enit.utm.tn>
2024-02-11 20:09:22 +0100

    done

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,6 +3,7 @@ import 'package:recipe_list/models/recipe.api.dart';
 import 'package:recipe_list/views/widgets/recipe_card.dart';
 
 import '../models/recipe.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -11,44 +12,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Recipe> _recipes = [];
+  bool _isLoading = true;
 
-  List<Recipe> _recipes=[];
-  bool _isLoading =true;
   @override
   void initState() {
-
     super.initState();
 
     getRecipes();
   }
-  Future<void> getRecipes() async {
-    _recipes =await RecipeApi.getRecipe();
-    setState(() {
-      _isLoading=false;
 
+  Future<void> getRecipes() async {
+    _recipes = await RecipeApi.getRecipe();
+    setState(() {
+      _isLoading = false;
     });
-    print(_recipes);
   }
 
-
-
   @override
-
-
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.restaurant_menu),
-            SizedBox(width: 10,),
-            Text('Omik Sanefa  '),
-            Text('🇹🇳',style: TextStyle(fontSize: 20),)
-          ],
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.restaurant_menu),
+              SizedBox(
+                width: 10,
+              ),
+              Text('Omik Sanefa  '),
+              Text(
+                '🇹🇳',
+                style: TextStyle(fontSize: 20),
+              )
+            ],
+          ),
         ),
-      ),
-      body: RecipeCard(title: "my recipe", cookTime: '30 min ', rating: '4.3', thumbnailUrl:'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360') ,
-    );
+        body: _isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: _recipes.length, itemBuilder: (context,index){
+                  return RecipeCard(title: _recipes[index].name, cookTime: _recipes[index].totalTime, rating: _recipes[index].rating.toString(), thumbnailUrl:_recipes[index].images);
+        }));
   }
 }
