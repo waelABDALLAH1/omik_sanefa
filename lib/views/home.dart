05ab18a064b5f876a7dd45b4e80074f5ebb1d447
wael <wael.abdallah@etudiant-enit.utm.tn>
2024-02-12 22:09:35 +0100

    done

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -7,7 +7,8 @@ import '../models/recipe.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
